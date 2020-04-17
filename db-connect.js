b38c34705a6c7cbe2b0bc883cadd76e80c5f6fac
Ben LaGrone <ben.lagrone@shell.com>
2020-04-17 17:15:27 -0500

    mongoose

--- db-connect.js
+++ db-connect.js
@@ -3,9 +3,22 @@ var mongoose = require('mongoose');
 var dbName = '/watchlistDB';
 var mongoDB = "mongodb://localhost:27017";
 
-mongoose.connect(mongoDB+dbName, {useNewUrlParser: true});
+mongoose.connect(mongoDB+dbName, {useNewUrlParser: true, useUnifiedTopology: true});
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Mongo connection error:'));
+
+db.once('open', function(){
+    console.log('open')
+})
+
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var silence = new Kitten({name: 'Silence'});
+console.log(silence.name);
 
